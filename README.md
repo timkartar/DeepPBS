53b19a44aa62f85d880e0f5943de0b3deb0f134d
timkartar <timkartar7879@gmail.com>
2023-05-05 13:59:16 -0700

    add readme and setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# geobind_dna
+
+Dependencies:
+
+* Biopython
+* datetime
+* matplotlib
+* scipy
+* scikit-learn
+* pytorch
+* pytorch_geometric
+* tqdm
+* glob
+* igl
+* json
+* logomaker
+* networkx
+* pandas
+* seaborn
+* trimesh
+* numpy
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deeppbs",
+    version="0.0.1",
+    author="Raktim Mitra, Jared Sagendorf",
+    author_email="jsagendorf@gmail.com",
+    description="Geometry-invariant deep learning on protein-DNA structures for interpretable prediction of binding specificity across protein families",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timkartar/DeepPBS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+	"Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
