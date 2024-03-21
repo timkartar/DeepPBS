2bfb3c599ec824dda60d997cf53df44e78339d41
Raktim Mitra <timkartar7879@gmail.com>
2024-03-21 15:59:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ setuptools.setup(
     classifiers=[
 	"Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
